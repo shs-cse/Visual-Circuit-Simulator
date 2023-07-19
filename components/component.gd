6613fe57c7fab13187d53cc9f0440bf5550aa4e9
Shadman Shahriar <shadman.shahriar@bracu.ac.bd>
2023-07-19 18:02:41 +0600

    si.gd & component.gd feels good

--- components/component.gd
+++ components/component.gd
@@ -26,49 +26,26 @@ const unit_dict := {
 @export var type := Component.TYPE.RESISTOR
 ## Just the numerical value of the quantity (e.g. -3.4)
 @export var numerical_value := NAN:
-	get:
-		return self.si.number if self.si else NAN
 	set(number):
-		if self.si:
-			var returned_values := SI.compact_notation(number, si.prefix)
-			if numerical_value != returned_values[0]:
-				numerical_value = returned_values[0]
-			if si_unit_prefix != returned_values[1]:
-				si_unit_prefix = returned_values[1]
-#			numerical_value = number
-#			self.si.number = self.numerical_value
-#			self.si.prefix = self.si_unit_prefix
-## Just the SI unit prefix of the quantity (e.g. Volt)
+		if self.si and numerical_value != number:
+			print("changing number of si quantity to ", number)
+			self.si.update_number(number)
+			numerical_value = self.si.number
+			# if number < 1 or number > 1e3, prefix might need updating
+			self.si_unit_prefix = self.si.prefix
+				
 @export var si_unit_prefix := SI.PREFIX.NONE:
 	set(prefix):
-		if self.si:
-			var returned_values := SI.compact_notation(si.number, prefix)
-			if numerical_value != returned_values[0]:
-				numerical_value = returned_values[0]
-			if si_unit_prefix != returned_values[1]:
-				si_unit_prefix = returned_values[1]
-#			si_unit_prefix = prefix
-#			self.si.number = self.numerical_value
-#			self.si.prefix = self.si_unit_prefix
-#@export var si_value
+		if self.si and si_unit_prefix != prefix:
+			print("changing prefix of si quantity to ", prefix)
+			self.si.update_prefix(prefix)
+			si_unit_prefix = self.si.prefix
 ## SI value, prefix and unit packaged together
 @onready var si := SI.new(
 	self.numerical_value, 
 	self.si_unit_prefix, 
 	unit_dict[self.type]
 )
-	
-
-
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-#	$BoundingBox.shape.size = self.size * TILE_SIZE
-#	self._si = SI.new(
-#		self.numerical_value, 
-#		self.si_unit_prefix, 
-#		unit_dict[self.type]
-#	)
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- si.gd
+++ si.gd
@@ -19,7 +19,7 @@ enum PREFIX {
 	FEMTO = -15
 }
 ## Dictionary mapping of unit enum to their string represtaion
-const UNIT_REPRE_DICT := {
+const UNIT_REPR_DICT := {
 	UNIT.OHM: "Ω",
 	UNIT.VOLT: "V",
 	UNIT.AMPERE: "A"
@@ -38,54 +38,58 @@ const PREFIX_REPR_DICT := {
 	PREFIX.FEMTO: "f"
 }
 ## Just the numerical part of the component value (without the SI unit/prefix)
-@export var number := NAN:
-	set(_number):
-		print("setting number of si quantity")
-		number = _number
-#		var returned_values := compact_notation(_number, prefix)
-#		if number != returned_values[0]:
-#			number = returned_values[0]
-#		if prefix != returned_values[1]:
-#			prefix = returned_values[1]
-		value = number * 10**prefix
-
+@export var number := NAN
 ## Just the SI unit prefix part of the component value (without the unit)
-@export var prefix := PREFIX.NONE:
-	set(_prefix):
-		print("setting prefix of si quantity")
-		prefix = _prefix
-#		var returned_values := compact_notation(number, _prefix)
-#		if number != returned_values[0]:
-#			number = returned_values[0]
-#		if prefix != returned_values[1]:
-#			prefix = returned_values[1]
-		value = number * 10**prefix
-		
+@export var prefix := PREFIX.NONE
 ## SI unit of the component
 @export var unit := UNIT.OHM
 ## Numerical value in SI unit
-var value := NAN:
-	set(_value):
-		print("trying to change %s" % _value)
+var value := NAN
 
 func _init(_number:float, _prefix:PREFIX, _unit:UNIT):
 	self.number = _number
 	self.prefix = _prefix
 	self.unit = _unit
 
-static func compact_notation(_number:float, _prefix:PREFIX) -> Array:
-	print("before: %3.2f %s" % [_number, PREFIX_REPR_DICT[_prefix]])
-	while absf(_number) >= 1e3 and _prefix < PREFIX.values().max():
-		_number /= 1e3
-		_prefix += 3
-	while absf(_number) < 1 and _prefix <= 0 and _prefix > PREFIX.values().min():
-		_number *= 1e3
-		_prefix -= 3
-#	self.number = _number
-#	self.prefix = _prefix
-#	self.value = number * 10**prefix
-	print("after: %s %s" % [_number, PREFIX_REPR_DICT[_prefix]])
-	return [_number, _prefix]
+func _to_string():
+	return "%.2f %s%s" % [
+		self.number, 
+		PREFIX_REPR_DICT[self.prefix], 
+		UNIT_REPR_DICT[self.unit]
+	]
 	
+
+func update_value():
+	self.value = self.number * 10.0 ** self.prefix
+	print_rich(
+		"new si unit value: ", 
+		"[color=magenta]", 
+		_to_string(),
+		"[/color] = ",
+		self.value
+	)
+	
+func update_prefix(_prefix:PREFIX):
+	self.prefix = _prefix
+	update_value()
+	
+func update_number(_number:float):
+	self.number = _number
+	print_rich("from: [color=pink]", self.number, PREFIX_REPR_DICT[self.prefix], "[/color]")
+	while absf(self.number) >= 1e3-1e-6 and self.prefix < PREFIX.values().max():
+		self.number /= 1e3
+		self.prefix += 3
+		print("maxi loop: ", self.number, "e", self.prefix)
+	# loop to find smaller prefixes	
+	while absf(self.number) < 1-1e-6 and self.prefix > PREFIX.values().min():
+		self.number *= 1e3
+		self.prefix -= 3
+		print("mini loop: ", self.number, "e", self.prefix)
+	# finally update value in SI unit
+	print_rich("to: [color=cyan]", self.number, PREFIX_REPR_DICT[self.prefix], "[/color]")
+	update_value()
+
+
+
 	
 	
